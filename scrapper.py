00fc1e32b2bd68b4e707f97f1b50b43bfcfcb7d8
bocchi277 <astra277353@gmail.com>
2026-03-02 20:14:01 +0530

    Remove other project and add selenium for dynamic website handling like sitare univerty website univ.sitare.org

---
+++ scrapper.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+def loadUrl(userUrl):
+    driver = webdriver.Chrome()
+    try:
+        driver.get(userUrl)
+        time.sleep(4)
+        content = driver.page_source
+        driver.quit()
+    except Exception:
+        return None
+    if content:
+        soup = BeautifulSoup(content, 'html.parser')
+        title_tag = soup.find('title')
+        title = title_tag.text if title_tag else "Title Tag is missing"
+        bodyTag = soup.find('body')
+        bodyText = bodyTag.get_text(separator="\n", strip=True) if bodyTag else "Body is Not there"
+        links = [link.get('href') for link in soup.find_all('a') if link.get('href')]
+        formatLinks = [i for i in links if i.startswith("https://")]
+        return {"title": title, "bodyText": bodyText, "Links": formatLinks}
+    else:
+        print("Some error happened!!")
+        return content
+if __name__ == "__main__":
+    try:
+        userUrl = sys.argv
+        if len(userUrl) == 2:
+                userUrl = sys.argv[1]
+                if not userUrl.startswith("https://"):
+                   userUrl = f"https://{userUrl}"
+                content = loadUrl(userUrl)
+                if content:
+                    for i in content:
+                        if i != "Links":
+                            print(f"The {i} is --> {content[i]}")
+                        else:
+                            print("The links are -->")
+                            for j in content[i]:
+                                print(j)
+                else:
+                    print("Failed , Empty Content")
+    except Exception as e:
+        print(e)
